48ccaef2ccb3227af345017950c8d115b52b947c
lukasheinrich <lukas.heinrich@gmail.com>
2017-02-18 13:21:14 +0100

    exception handling...

--- packtivity_reana_backend/reana_interface.py
+++ packtivity_reana_backend/reana_interface.py
@@ -20,40 +20,42 @@ def submit(experiment, image, cmd, cvmfs = False, grid = False):
     if cvmfs:
         job_spec['cvmfs_mounts'] = ['atlas-condb', 'atlas','sft']
         
-
     log.info('submitting %s',json.dumps(job_spec, indent = 4, sort_keys = True))
 
-    try:
-        response = requests.post(
-            'http://{host}/{resource}'.format(
-                host=JOBCONTROLLER_HOST,
-                resource='jobs'
-            ),
-            json=job_spec,
-            headers={'content-type': 'application/json'}
-        )
-    except requests.exceptions.ConnectionError:
-        log.info('caught ConnectionError in submit.. ignore')
-        pass
-
-    job_id = str(response.json()['job-id'])
-    return job_id
+    for submit_try in range(10):
+        try:
+            response = requests.post(
+                'http://{host}/{resource}'.format(
+                    host=JOBCONTROLLER_HOST,
+                    resource='jobs'
+                ),
+                json=job_spec,
+                headers={'content-type': 'application/json'}
+            )
+            job_id = str(response.json()['job-id'])
+            return job_id
+        except requests.exceptions.ConnectionError:
+            log.info('caught ConnectionError in submit try: %s.. try again in 10 seconds..',submit_try)
+            pass
+        time.sleep(10)
 
 def check_status(job_id):
     time.sleep(BUFFER_TIME) #buffers to not hammer server when this is called in a tight loop
 
-    try:
-        response = requests.get(
-            'http://{host}/{resource}/{id}'.format(
-                host=JOBCONTROLLER_HOST,
-                resource='jobs',
-                id=job_id
-            ),
-            headers = {'cache-control':'no-cache'}
-        )
-    except requests.exceptions.ConnectionError:
-        log.info('caught ConnectionError in check_status.. ignore')
-        pass
-    
-    job_info = response.json()['job']
-    return job_info
+    for check_try in range(10):
+        try:
+            response = requests.get(
+                'http://{host}/{resource}/{id}'.format(
+                    host=JOBCONTROLLER_HOST,
+                    resource='jobs',
+                    id=job_id
+                ),
+                headers = {'cache-control':'no-cache'}
+            )
+            job_info = response.json()['job']
+            return job_info
+        except requests.exceptions.ConnectionError:
+            log.info('caught ConnectionError in check_status try %s.. try again in 10 seconds',check_try)
+            pass
+        time.sleep(10)
+
